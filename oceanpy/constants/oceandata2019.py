fc30a47b7aea500c3816e88a4617ec4815722175
profxj <xavier@ucolick.org>
2019-11-25 10:03:20 -0800

    this is the right branch

--- oceanpy/constants/oceandata2019.py
+++ oceanpy/constants/oceandata2019.py
@@ -16,3 +16,7 @@ unyt.define_unit('T_earth', T_earth)
 # Rotation rate of the Earth
 Omega_earth = 2*np.pi/T_earth
 unyt.define_unit('Omega_earth', Omega_earth)
+
+# Density of water -- T=25 deg C, S=35 g/kg, P = 1 atm
+density_seawater = 1023.6 * unyt.kg / unyt.m**3
+unyt.define_unit('density_seawater', density_seawater)
